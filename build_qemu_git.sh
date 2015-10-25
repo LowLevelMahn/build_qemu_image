f379cf97aba7ccd2022e67a346359a85746720fb
dennis luehring <dl.soluz@gmx.net>
2015-10-25 15:48:45 +0100

    add last working tag - something change in head with qemu parameter monitor/serial

--- build_qemu_git.sh
+++ build_qemu_git.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 rm qemu -rf
-git clone git://git.qemu-project.org/qemu.git
+git clone -b "v2.4.0.1" git://git.qemu-project.org/qemu.git
+#git clone git://git.qemu-project.org/qemu.git
 cd qemu
 ./configure --target-list=sparc64-softmmu,alpha-softmmu,mips64-softmmu
 make -j5
